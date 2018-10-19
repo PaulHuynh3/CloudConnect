e5ff00c7b495c098fa8061bfe2a34d6237a445b2
paulhuynh <paul.huynh3@gmail.com>
2018-10-19 17:03:40 -0400

    first commit

---
+++ CloudConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudConnect
+//
+//  Created by Paul on 2018-10-19.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
