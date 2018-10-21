47aab448fceb49cf4149f23a0199a63d435ea673
paulhuynh <paul.huynh3@gmail.com>
2018-10-21 15:40:19 -0400

    create the ui

--- CloudConnect/Controller/ServerViewController.swift
+++ CloudConnect/Controller/ServerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ServerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +16,21 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+}
+
+extension ServerViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
+    
 }
 
